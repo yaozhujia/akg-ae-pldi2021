6fe9b1bd96ff103978cbc8e588583626acf5b6fb
anyrenwei <zhangrenwei1@huawei.com>
2021-03-02 21:01:35 +0800

    upload whl and runtime env

--- test_env.sh
+++ test_env.sh
@@ -1,5 +1,13 @@
-redis_server_ip: 121.37.54.17
-redis_server_port: 6380
-pipline_fronted_ip: 121.37.54.17
-pipline_fronted_port: 2030
-wait_timeout: 300
+pip_show_str=`pip3 show akg`
+str1=${pip_show_str#*Location:}
+str2=${str1%Requires*}
+str3=${str2%?}
+AKG_ROOT=${str3#* }
+TVM_ROOT="${AKG_ROOT}/third_party/incubator-tvm"
+LIBAKG_ROOT="${AKG_ROOT}/build"
+export LD_LIBRARY_PATH=${LIBAKG_ROOT}:${LD_LIBRARY_PATH}
+export PYTHONPATH=${TVM_ROOT}/python:${TVM_ROOT}/topi:${TVM_ROOT}/topi/python:${AKG_ROOT}/tests/common:${AKG_ROOT}/python:${AKG_ROOT}/tests/fuzz/tune:${PYTHONPATH}
+
+export RUNTIME_MODE=air_cloud
+export PROFILING=true
+
